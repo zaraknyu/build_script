3ddb372e4f146c3867a903c32436c7c109534cb4
ZaraKinYu <zarakinyu@outlook.in>
2025-12-13 02:34:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,11 +8,17 @@ rm -rf device/xiaomi/warm-kernel
 rm -rf hardware/xiaomi
 rm -rf hardware/qcom-caf/common
 
-git clone https://github.com/zarakinyu-playground/device_xiaomi_warm device/xiaomi/warm
-git clone https://github.com/zarakinyu-playground/vendor_xiaomi_warm vendor/xiaomi/warm
+
 git clone https://github.com/zarakinyu-playground/android_device_xiaomi_warm-kernel device/xiaomi/warm-kernel
 git clone https://github.com/zarakinyu-playground/android_hardware_qcom-caf_common -b lineage-23.0 hardware/qcom-caf/common
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi
+
+source build/envsetup.sh
+lunch lineage_warm-bp2a-userdebug
+mka bacon
+
+git clone https://github.com/zarakinyu-playground/android_device_xiaomi_warm device/xiaomi/warm
+git clone https://github.com/zarakinyu-playground/android_vendor_xiaomi_warm vendor/xiaomi/warm
 
 source build/envsetup.sh
 lunch lineage_warm-bp2a-userdebug
